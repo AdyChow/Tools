70665dc44705a82b7606b350a20f6e821c98b6c3
zhouxinyuan <zhouxinyuan@hellogroup.com>
2025-02-15 20:05:45 +0800

    在 xml 中声明二维码扫描 view

--- app/src/main/java/com/ady/tools/pages/ScanCodeAct.kt
+++ app/src/main/java/com/ady/tools/pages/ScanCodeAct.kt
@@ -12,6 +12,7 @@ import com.google.zxing.integration.android.IntentIntegrator
 class ScanCodeAct : AppCompatActivity() {
 
     private lateinit var binding: ActScanCodeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActScanCodeBinding.inflate(layoutInflater)
@@ -19,14 +20,31 @@ class ScanCodeAct : AppCompatActivity() {
         initScan()
     }
 
-    private fun initScan() { // 启动内置扫描
-        val integrator = IntentIntegrator(this);
-        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
-        integrator.setPrompt("Scan a QR Code");
-        integrator.setCameraId(0);
-        integrator.setBeepEnabled(false);
-        integrator.setBarcodeImageEnabled(true);
-        integrator.initiateScan();
+    private fun initScan() { // 启动扫描
+        binding.scanView.decodeContinuous { result ->
+            handleScannedUrl(result.text)
+        }
+        binding.scanView.resume() // 开始扫描（在已获得相机权限时调用）
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.scanView.resume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.scanView.pause()
+    }
+
+    private fun initInternalScan() { // 启动内置扫描
+        val integrator = IntentIntegrator(this)
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+        integrator.setPrompt("Scan a QR Code")
+        integrator.setCameraId(0)
+        integrator.setBeepEnabled(false)
+        integrator.setBarcodeImageEnabled(true)
+        integrator.initiateScan()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -38,16 +56,20 @@ class ScanCodeAct : AppCompatActivity() {
                 binding.scanResult.text = ""
             } else {
                 Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show();
-                binding.scanResult.text = result.contents
-                kotlin.runCatching {
-                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(result.contents))
-                    startActivity(intent)
-                }.onFailure {
-                    it.printStackTrace()
-                }
+                handleScannedUrl(result.contents)
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private fun handleScannedUrl(result: String?) {
+        binding.scanResult.text = result
+        kotlin.runCatching {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(result))
+            startActivity(intent)
+        }.onFailure {
+            it.printStackTrace()
         }
     }
 }
